4a908bd530e74555d3475f76c2792e2f3dbf9dd2
Rehan Qasim <rehanbhatti0317@gmail.com>
2022-08-16 19:27:08 +0500

    Added Product detail page with designed cover

--- src/components/DetailItem/ProductDetail.js
+++ src/components/DetailItem/ProductDetail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import useApi from "../../hooks/useApi";
@@ -9,6 +10,7 @@ const getProductDetail = (id) => {
 };
 export default function ProductDetail(props) {
   const { id } = useParams();
+  const [isClicked, setIsClicked] = React.useState(false);
   const response = useApi(getProductDetail);
   const data = response.data;
   useEffect(() => {
@@ -16,13 +18,48 @@ export default function ProductDetail(props) {
   }, []);
   console.log(id);
   return (
-    <div className=" bg-purple-600 border-2 text-center w-1/2 table-center text-white border-purple-900 mt-5 hover:bg-purple-200 hover:text-purple-800">
-      <h1 className="font-bold text-2xl ">{data.title}</h1>
-      <h1 className="font-bold text-xl"> {data.description}</h1>
-      <h1 className="font-bold text-xl"> {data.price}$</h1>
-      <h1 className="font-bold text-xl"> {data.tax}$</h1>
-      <div className="font-bold text-xl text-center table p-2 border-black border-2 rounded-lg mx-auto">
-        {data.inventory}
+    <div className="bg-slate-100 py-7 px-16 h-screen">
+      <div className="bg-zinc-50 put-shadow flex gap-2 mx-auto rounded-md px- w-3/4">
+        <div className="product-img">
+          <img
+            className="h-full"
+            src="https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?cs=srgb&dl=pexels-eprism-studio-335257.jpg&fm=jpg"
+            alt=""
+          />
+        </div>
+
+        <div className="p-7 basis-2/5">
+          <h1 className="font-extrabold text-3xl text-center">{data.title}</h1>
+          <h2 className="font-normal text-xl"> ${data.price}</h2>
+          <hr />
+          <div className="flex justify-between my-2">
+            <p className="text-gray-800">Description</p>
+            <p
+              className="text-xl font-bold cursor-pointer"
+              onClick={() => setIsClicked((old) => !old)}
+            >
+              +
+            </p>
+          </div>
+          {isClicked && <p>{data.description}</p>}
+          <hr />
+
+          <div className="flex items-center py-2">
+            <h3 className="italic  ">Stock Available</h3>
+
+            <div className="font-medium italic text-center  table p-1 border-black border-2 rounded-lg ml-3">
+              {data.inventory}
+            </div>
+          </div>
+
+          <div className="quantity flex gap-2">
+            <div className="font-medium italic text-center text-3xl px-3  table p-1 border-black border-2 rounded-full ml-3">
+              -
+            </div>{" "}
+            <input type="text" className="w-10 text-center font-extrabold" />
+            <button className="btn text-2xl rounded-full">+</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import { authActions } from "../../store/Slices/AuthSlice";
 import classes from "./MainNavigation.module.css";
 import { useSelector, useDispatch } from "react-redux";
-
 const MainNavigation = () => {
   const idToken = useSelector((state) => state.Auth.idToken);
   const authDispatch = useDispatch();
